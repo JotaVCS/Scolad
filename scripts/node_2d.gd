c74b8327c9bf7ff54eb7dfb8ffa2f7231f06fa25
JotaVCS <joao.26006@aluno.iffar.edu.br>
2026-05-05 11:05:32 -0300

    Ultima Att

--- scripts/node_2d.gd
+++ scripts/node_2d.gd
@@ -3,11 +3,17 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Dialogic
+	Dialogic.timeline_ended.connect(_on_timeline_ended)
+	Dialogic.signal_event.connect(_on_signal)	
 	Dialogic.start("Timeline1")
+	
 	pass # Replace with function body.
 
-
+func _on_timeline_ended():
+	get_tree().quit()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_signal(signal_passed_in):
+	match signal_passed_in:
+		"fim":
+			pass
